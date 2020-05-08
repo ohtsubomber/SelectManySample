8924ad477b4d6705664f72a5990aeee04f340232
ohtsubo_masakazu <ohtsubo-masakazu@edu.brain.kyutech.jp>
2020-05-08 13:24:58 +0900

    SelectMany サンプルを作成

--- SelectManySample/Program.cs
+++ SelectManySample/Program.cs
@@ -10,6 +10,24 @@ namespace SelectManySample
     {
         static void Main(string[] args)
         {
+            var data1 = new Data() { List = new List<string>() { "1", "2" } };
+            var data2 = new Data() { List = new List<string>() { "3", "4" } };
+            var dataList = new List<Data>() { data1, data2 };
+
+            //data1とdata2のListを結合したListを作成
+            var marge = dataList.SelectMany(data => data.List);
+
+            foreach(var val in marge)
+            {
+                Console.WriteLine($"{val}");
+            }
+
+            Console.ReadLine();
         }
+    }
+
+    class Data
+    {
+        public List<string> List { get; set; }
     }
 }
